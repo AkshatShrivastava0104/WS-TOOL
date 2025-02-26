659d697546e0400525aa210df0ac3424e8bcea0f
Akshat <121187184+AkshatShrivastava0104@users.noreply.github.com>
2025-02-26 22:39:20 +0530

    Update wc_tool.cpp

--- wc_tool.cpp
+++ wc_tool.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "wc_tool.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {  
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
         return 1;
